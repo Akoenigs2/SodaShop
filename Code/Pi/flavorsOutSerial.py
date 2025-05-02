44dda5dfc82113d9cc6381d8dd4c7ca5055d15b9
Nicholas Witulski <nwitulski2@huskers.unl.edu>
2025-05-02 08:37:54 -0500

    Bug fixes v3

--- Code/Pi/flavorsOutSerial.py
+++ Code/Pi/flavorsOutSerial.py
@@ -3,49 +3,6 @@ import serial
 from guizero import App
 
 def sendFlavorValues(selection):
-  def mapFlavorValues():
-    dispensingDrinkFlavors = [dispensingDrink.flavor1Name, dispensingDrink.flavor2Name, dispensingDrink.flavor3Name, dispensingDrink.flavor4Name]
-    chosenFlavorNames = parms.getListOfChosenFlavorNames()
-    for i, drinkFlavor in enumerate(dispensingDrinkFlavors):
-      for j, selectedFlavor in enumerate(chosenFlavorNames):
-        if drinkFlavor == selectedFlavor:
-          if j == 1:
-            if (i == 1):
-              parms.modify_value(p1, dispensingDrink.flavor1Perc)
-            elif (i == 2):
-              parms.modify_value(p1, dispensingDrink.flavor2Perc)
-            elif (i == 3):
-              parms.modify_value(p1, dispensingDrink.flavor3Perc)
-            elif (i == 4):
-              parms.modify_value(p1, dispensingDrink.flavor4Perc)
-          elif j == 2:
-            if (i == 1):
-              parms.modify_value(p2, dispensingDrink.flavor1Perc)
-            elif (i == 2):
-              parms.modify_value(p2, dispensingDrink.flavor2Perc)
-            elif (i == 3):
-              parms.modify_value(p2, dispensingDrink.flavor3Perc)
-            elif (i == 4):
-              parms.modify_value(p2, dispensingDrink.flavor4Perc)
-          elif j == 3:
-            if (i == 1):
-              parms.modify_value(p3, dispensingDrink.flavor1Perc)
-            elif (i == 2):
-              parms.modify_value(p3, dispensingDrink.flavor2Perc)
-            elif (i == 3):
-              parms.modify_value(p3, dispensingDrink.flavor3Perc)
-            elif (i == 4):
-              parms.modify_value(p3, dispensingDrink.flavor4Perc)
-          elif j == 4:
-            if (i == 1):
-              parms.modify_value(p4, dispensingDrink.flavor1Perc)
-            elif (i == 2):
-              parms.modify_value(p4, dispensingDrink.flavor2Perc)
-            elif (i == 3):
-              parms.modify_value(p4, dispensingDrink.flavor3Perc)
-            elif (i == 4):
-              parms.modify_value(p4, dispensingDrink.flavor4Perc)
-
   dispensingDrink = parms.currentDrink['value']
   if selection != "custom":
     dispensingDrink = parms.favoriteDrinks[int(selection)]
@@ -60,7 +17,48 @@ def sendFlavorValues(selection):
   p4 = {'value':0}
   p5 = {'value':0}
 
-  mapFlavorValues()
+  dispensingDrinkFlavors = [dispensingDrink.flavor1Name, dispensingDrink.flavor2Name, dispensingDrink.flavor3Name, dispensingDrink.flavor4Name]
+    
+  chosenFlavorNames = parms.getListOfChosenFlavorNames()
+  for i, drinkFlavor in enumerate(dispensingDrinkFlavors):
+    for j, selectedFlavor in enumerate(chosenFlavorNames):
+      if drinkFlavor == selectedFlavor:
+        if j == 1:
+          if (i == 1):
+            parms.modify_value(p1, dispensingDrink.flavor1Perc)
+          elif (i == 2):
+            parms.modify_value(p1, dispensingDrink.flavor2Perc)
+          elif (i == 3):
+            parms.modify_value(p1, dispensingDrink.flavor3Perc)
+          elif (i == 4):
+            parms.modify_value(p1, dispensingDrink.flavor4Perc)
+        elif j == 2:
+          if (i == 1):
+            parms.modify_value(p2, dispensingDrink.flavor1Perc)
+          elif (i == 2):
+            parms.modify_value(p2, dispensingDrink.flavor2Perc)
+          elif (i == 3):
+            parms.modify_value(p2, dispensingDrink.flavor3Perc)
+          elif (i == 4):
+            parms.modify_value(p2, dispensingDrink.flavor4Perc)
+        elif j == 3:
+          if (i == 1):
+            parms.modify_value(p3, dispensingDrink.flavor1Perc)
+          elif (i == 2):
+            parms.modify_value(p3, dispensingDrink.flavor2Perc)
+          elif (i == 3):
+            parms.modify_value(p3, dispensingDrink.flavor3Perc)
+          elif (i == 4):
+            parms.modify_value(p3, dispensingDrink.flavor4Perc)
+        elif j == 4:
+          if (i == 1):
+            parms.modify_value(p4, dispensingDrink.flavor1Perc)
+          elif (i == 2):
+            parms.modify_value(p4, dispensingDrink.flavor2Perc)
+          elif (i == 3):
+            parms.modify_value(p4, dispensingDrink.flavor3Perc)
+          elif (i == 4):
+            parms.modify_value(p4, dispensingDrink.flavor4Perc)
   
   # TODO: Uncomment when on Pi
   SERIAL_PORT = '/dev/ttyACM0'
@@ -68,7 +66,7 @@ def sendFlavorValues(selection):
 
   try:
     with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
-      data = f"{p1},{p2},{p3},{p4},{p5}\n"
+      data = f"{p1['value']},{p2['value']},{p3['value']},{p4['value']},{p5['value']}\n"
       ser.write(data.encode('utf-8'))
       print(f"Sent: {data.strip()}")
   except serial.SerialException as e:
